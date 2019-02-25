eaaae79e3fc5c459161f51178353b3f772a502fa
Nicolas <nicoga97@gmail.com>
2019-02-24 21:34:05 -0500

    Added app Drawer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Login from "./Login/Login"
+import Login from "./Components/Login"
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import NavigationDrawer from "./Components/NavigationDrawer";
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Login/>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <div className="App">
+                        <header className="App-header">
+
+                            <Route path="/mainView" component={NavigationDrawer}/>
+                            <Route exact path="/" component={Login}/>
+                        </header>
+                    </div>
+
+                </div>
+            </Router>
+
+        );
+    }
 }
 
 export default App;
